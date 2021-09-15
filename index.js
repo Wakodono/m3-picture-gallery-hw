71cdfb9e40319b8a97bf229089afc45344f51092
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-09-15 16:05:46 +0100

    Images load on click

--- index.js
+++ index.js
@@ -12,7 +12,42 @@ const primaryButton = document.querySelector("load-images")
 const loadImages = (query) => {
     fetch("https://api.pexels.com/v1/search?query=" + query, requestOptions)
       .then(response => response.json())
-      .then(result => console.log(result))
+      .then(result => {
+          const row = document.querySelector("main .row")
+          row.innerHTML = ""
+
+          console.log(row)
+
+        
+           for (let photo of result.photos) {
+               console.log(photo)
+           
+              row.innerHTML += `
+              <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img src=${photo.src.small} width = 287 height = 225 ">
+                <div class="card-body">
+                  <p class="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">
+                        View
+                      </button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">
+                        Edit
+                      </button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>`
+          }
+      })
       .catch(error => console.log('error', error));
 
 }
